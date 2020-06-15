fba9ecf4070529686588c099be8732e892a28a75
Golden Eagle <joowoon.kang@biola.edu>
2020-06-15 15:33:41 -0700

    finished searching

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,5 +1,7 @@
 def linear_search(arr, target):
-    # Your code here
+    for i in (range(len(arr))):
+        if arr[i] == target:
+            return i
 
 
     return -1   # not found
@@ -7,8 +9,23 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    first = 0
+    last = (len(arr) - 1)
 
-    # Your code here
+    found = False
+
+    while first <= last and not found:
+        #first using first digit division
+        middle = (first + last)//2
+
+        if arr[middle] == target:
+            return middle
+
+        else:
+            if target < arr[middle]:
+                last = middle -1
+            else:
+                first = middle + 1
 
 
     return -1  # not found
