01459a63eadd726ccea093fc60f056ebce26048b
Golden Eagle <joowoon.kang@biola.edu>
2020-06-15 18:38:14 -0700

    finished iterative

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,3 +1,20 @@
+def insertion_sort(arr):
+    # Traverse through 1 to len(arr) 
+    for i in range(1, len(arr)): 
+  
+        current_item = arr[i] 
+  
+        # Move elements of arr[0..i-1], that are 
+        # greater than key, to one position ahead 
+        # of their current position 
+        prev_indx = i-1
+        while prev_indx >=0 and current_item < arr[prev_indx] : 
+                arr[prev_indx+1] = arr[prev_indx] 
+                prev_indx -= 1
+        arr[prev_indx+1] = current_item 
+
+# insertion_sort([4,2,10,6,1])
+
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
@@ -7,19 +24,25 @@ def selection_sort(arr):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
-
+        for item in range(cur_index, len(arr)):
+            if arr[smallest_index] > arr[item]:
+                smallest_index = item
         # TO-DO: swap
         # Your code here
+        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]        
 
     return arr
+# [3, 5, 8, 4, 2, 9, 6, 0, 3, 7]
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
-
+    current_index = len(arr)
+    for index in range(current_index-1):
+        for compare in range(0,current_index-index-1):
+            if arr[compare] > arr[compare+1] :
+                arr[compare], arr[compare+1] = arr[compare+1], arr[compare]
     return arr
 
 '''
